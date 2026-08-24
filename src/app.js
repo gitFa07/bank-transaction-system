75708a99021c51d5bd8381cb58e241b874636351
gitFa07 <office.falakanwar@gmail.com>
2026-08-25 01:09:40 +0530

    Cors implementation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
